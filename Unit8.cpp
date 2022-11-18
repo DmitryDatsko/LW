9c8dd5ccfa21c9931911c6e2bcc4fcf6848ba36b
DmitryDatsko <113901025+DmitryDatsko@users.noreply.github.com>
2022-11-18 16:46:29 +0200

    Update Unit8.cpp

--- Unit8.cpp
+++ Unit8.cpp
@@ -7,57 +7,48 @@ using namespace std;
 
 class LW8 {
 public:
-    static int Task1() {
+    static void Task1() {
         double x;
         std::cout << "Please enter a value of x: ";
         std::cin >> x;
         if (x > 1 && x < 1.5) {
-            std::cout << "First if";
             std::cout << "Y equal: " << (pow(x, 1 / 4)) + exp(pow(x, 2));
-            return 0;
         }
-        if (x > 0 && x <= 1) {
+        else if (x > 0 && x <= 1) {
             std::cout << "Y equal: " << sqrt(log(fabs(x)));
-            return 0;
         }
         else {
-            std::cout << "Else";
             std::cout << "Y equal: " << (5 + sqrt(fabs(x))) / (2 + 2 * x);
-            return 0;
         }
     }
 };
 
 class LW9 {
 public:
-    static int Task1() {
+    static void Task1() {
         double a = 1.24, b = 0.67, x_n = 10.2, x_k = 12.4, h = 0.43, y = 0, x;
         for (x = x_n; x <= x_k; x += h)
         {
             y = (a + pow(x, 1 / 3)) / pow(sin(b * x), 2);
         }
         std::cout << "Y equal: " << y;
-        return 0;
     }
 };
 
 class LW10 {
 public:
-    static int Task1(int A[2], int B[2], int C[2], int A1[2], int B1[2], int C1[2]) {
+    static void Task1(int A[2], int B[2], int C[2], int A1[2], int B1[2], int C1[2]) {
         double S1, S2;
         S1 = (((A[0] - C[0]) * (B[1] - C[1])) - ((B[0] - C[0]) * (A[1] - C[1]))) / 2.0;
         S2 = (((A1[0] - C1[0]) * (B1[1] - C1[1])) - ((B1[0] - C1[0]) * (A1[1] - C1[1]))) / 2.0;
         if (S1 > S2) {
             std::cout << "S1 biggest than S2 and equal: " << S1;
-            return 0;
         }
-        if (S1 == S2) {
+        else if (S1 == S2) {
             std::cout << "S1 equal S2: " << S1;
-            return 0;
         }
         else {
             std::cout << "S2 biggest than S1 and equal: " << S2;
-            return 0;
         }
     }
 };
@@ -157,7 +148,8 @@ public:
 };
 int main()
 {
-    //LW9::ConditionalStatements();
+    //LW8::Task1();
+    /*LW9::Task1();*/
     //LW10::LoopStatements();
     /*int A[2] = { 1, 3 };
     int B[2] = { 2, -5 };
@@ -168,7 +160,7 @@ int main()
     LW10::Task1(A, B, C, A1, B1, C1);*/
     //LW11::Task2();
     //LW12::Task1();
-    LW13::Task1();
+    //LW13::Task1();
     
 }
 
